97f57e55cca8b15ea38d13b8c9d9296e64062ffa
prashanth-g <prashanthcsgce@gmail.com>
2018-12-26 21:13:36 +0530

    Added arrows methods

--- arrows.js
+++ arrows.js
@@ -1,4 +1,4 @@
-// arrows in action
+// arrows in action - 01
 
 var animations = [
     'slideUp',
@@ -12,3 +12,16 @@ console.log(
         animation => animation.length
     )
 );
+
+// arrows as methods
+console.log("########### arrows in methods ############")
+var func = {
+    a: 10,
+    b: () => console.log(this.a, this),
+    c: function() {
+      console.log(this.a, this);
+    }
+  }
+  
+func.b(); 
+func.c(); // prints 10, Object {...}
