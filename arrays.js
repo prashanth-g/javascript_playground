916c129fc0eb83279f97bddf58ef577dafd48081
prashanth-g <prashanthcsgce@gmail.com>
2018-12-28 20:07:50 +0530

    Added arrays destructuring with nested objects - 02

--- arrays.js
+++ arrays.js
@@ -7,3 +7,29 @@ var a, b, remaining;
 console.log(a);  
 console.log(b); 
 console.log(remaining);  
+
+// Destructuring nested objects
+
+const response = {
+    status: 'true',
+    user: [
+       {
+        name: 'jlo',
+        available: true,
+        last_seen: '2018-12-27T01:35:21',
+       }
+    ],
+    message: 'success'
+};
+
+let {
+    status: responseCode, 
+    user: [
+        {
+            name: fullName, 
+        },
+    ],
+} = response;
+
+console.log(responseCode);  
+console.log(fullName); 
