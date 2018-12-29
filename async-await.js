645699ba4c7660d379d6a9dffdce52bbd8a13f3b
prashanth-g <prashanthcsgce@gmail.com>
2018-12-29 22:17:21 +0530

    Added async await - 02 concurrent start

--- async-await.js
+++ async-await.js
@@ -15,3 +15,33 @@ var add = async function(time, input) {
 add(2000, 10).then(v => {
     console.log(v);
 });
+
+// concurrent start
+var resolveIn3Seconds = function() {
+    console.log("starting resolveIn3Seconds");
+    return new Promise(resolve => {
+      setTimeout(function() {
+        console.log("slow resolveIn3Seconds is done");
+      }, 3000);
+    });
+};
+  
+var resolveIn1Second = function() {
+    console.log("starting resolveIn1Second");
+    return new Promise(resolve => {
+      setTimeout(function() {
+        console.log("slow resolveIn1Second is done");
+      }, 1000);
+    });
+};
+  
+var concurrentStart = async function() {
+
+    const slow = resolveIn3Seconds();  
+    const fast = resolveIn1Second();
+  
+    console.log(await slow);
+    console.log(await fast);
+}
+
+concurrentStart();
